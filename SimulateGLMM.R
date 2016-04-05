4bf41570ef01e193dcdb41af470c9aa9461e86dd
Meagan F. Oldfather <meagan_oldfather@berkeley.edu>
2016-04-05 11:33:40 -0700

    Update of GLMM simulation code
    The code now generates a list of Y values. I am still working on how to create the proper data frame, and also had to include variable numbers of covariates

--- SimulateGLMM.R
+++ SimulateGLMM.R
@@ -1,14 +1,17 @@
 # Intent: Simulate GLMM data
-
- eta=.5
-simulateGLMMM<-function(eta, ngroup, nblock,nsample, ncovs){
+simulateGLMM<-function(ngroup=2, nblock=4, nsample=140, alpha=7, beta1=.05, sd=.01){
   
-  Y<-data.frame(rep(NA,nsample))
-  Y<-rpois(nsample, lambda=eta)
-    
-  for(i in 1:nblock){
-  REsigma[i]<-dunif(x=nsample,0,1000)
-  }
-}
- 
- Ni <- rpois(50, lambda = 4)
+  # generate values of group covariate
+  group <-1:ngroup
+  
+  # generate random effect of block
+  r.block <-rnorm(n=nblock,mean=0,sd=sd)
+  
+  eta<-exp(alpha+beta1*group+r.block)
+  
+  Y<-rpois(nsample, lambda=eta) 
+  
+  return(list(ngroup=ngroup, nblock=nblock, alpha=alpha, beta1=beta1, sd=sd, eta=eta, Y=Y))
+    }
+
+simulateGLMM()
