3e17877a18aa3b82a3b5abebae0f4f0fff93c19f
MeaganOldfather <meagan_oldfather@berkeley.edu>
2016-04-06 11:34:08 -0700

    Added Sample Number to data

--- SimulateGLMM.R
+++ SimulateGLMM.R
@@ -15,7 +15,7 @@ simulateGLMM<-function(ngroup=2, nblock=4, reps.per.group = 20, alpha=7, sd.beta
   beta<-rnorm(n=ngroup,mean=0,sd=sd.beta)
 
   # I think we want the groups and the blocks crossed. -Perry
-  data<- data.frame(Group=rep(group, (nsample/ngroup)) , Block= rep(block, each=(nsample/nblock)), Eta=NA, Y=NA)
+  data<- data.frame(Sample.Num=rep(1:reps.per.group,times=(nsample/nblock)), Group=rep(group, (nsample/ngroup)) , Block = rep(block, each=(nsample/nblock)), Eta=NA, Y=NA)
 
   # Ok and simplest for now to simulate data here but also will be possible to do it through nimble later. -Perry
   # generate random effect of block
@@ -29,6 +29,8 @@ simulateGLMM<-function(ngroup=2, nblock=4, reps.per.group = 20, alpha=7, sd.beta
     }
 
 sim.glm<-simulateGLMM()
+head(sim.glm$data)
+table(sim.glm$data$Group, sim.glm$data$Block)
 
 #library(lme4)
 sim.glm$data$Group <-factor(sim.glm$data$Group)
@@ -36,5 +38,4 @@ glmm.fit <-glmer(Y~Group+(1|Block),family="poisson", data=sim.glm$data)
 summary(glmm.fit)
 
 ### Attempt to Simulate with Nimble ###
-library
-
+library("Nimble")
