3ad1332214a364532911eda066c305a530413322
arzeilinger <arzeilinger@gmail.com>
2016-04-05 22:37:53 -0700

    Now makeEcoDataMatrix() is more general.

--- functions_glmm_analysis.R
+++ functions_glmm_analysis.R
@@ -8,9 +8,17 @@ standardize <- function(covar){
 }
 
 # Make data matrices for JAGS GLMM with spatial random effect
-makeEcoDataMatrix <- function(var, data = detectData, fill = NA){
-  dataf <- data[,c("ymo","cellID",var)]
-  dataMatrix <- eval(substitute(spread(dataf, key = cellID, value = y, fill = fill), 
-                                list(y = as.name(names(dataf)[3]))))
+makeEcoDataMatrix <- function(var, rep = "ymo", reVar = "cellID", data = countData, fill = NA){
+  # This function takes a column (vector) of data and creates a matrix,
+  # with spatial cells going across the top, and year-months going down
+  # var = the variable (either explanatory or response)
+  # rep = column identifying replicates
+  # reVar = column identifying the random effect variable
+  # data = the dataset to transform
+  # fill = what should missing cells be filled with?
+  dataf <- data[,c(rep, reVar, var)]
+  dataMatrix <- eval(substitute(spread(dataf, key = key, value = y, fill = fill), 
+                                list(key = as.name(names(dataf)[2]),
+                                  y = as.name(names(dataf)[3]))))
   return(as.matrix(dataMatrix[,-1]))
 }
